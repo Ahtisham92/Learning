5271b0ae0e49651f1663f9548ed4612570e0a5e9
Ahtisham Ul Haq <ahtisham388@outlook.com>
2022-06-23 23:00:04 +0500

    added counter component

---
+++ src/component/Counter.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+import React from 'react'
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [bool, setBool] = useState(false);
+
+  return (
+    <div className="counter">
+      <button onClick={() => setBool(!bool)} >Re-Render</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <p>Count: {count}</p>
+    </div>
+  )
+}
+
+export default Counter
